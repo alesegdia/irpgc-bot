79f2350ac66b3f982574fbb744abe5d9312925cb
Alejandro Seguí <alesegdia@gmail.com>
2016-09-20 23:37:01 +0200

    wrapping prints in changeable functions

--- botirc.lua
+++ botirc.lua
@@ -28,8 +28,16 @@ local botirc = {
 
   -- login with proper nick and user
   login = function( self )
-	self:send_msg("USER " .. botdata.login .. " 8 * :" .. botdata.name)
-	self:send_msg("NICK " .. botdata.nick)
+    self:send_msg("USER " .. botdata.login .. " 8 * :" .. botdata.name)
+    self:send_msg("NICK " .. botdata.nick)
+  end,
+
+  notify_privmsg = function(msg)
+    print(msg)
+  end,
+  
+  notify_unformatted = function(msg)
+    print(msg)
   end,
 
   -- IRC base layer
@@ -43,7 +51,7 @@ local botirc = {
 	  if channel == botdata.nick then
 		print ("IGNORE: " .. nick .. ": " .. msg )
 	  else
-		print ("PRIVMSG: <" .. nick .. "> " .. msg )
+		self.notify_privmsg ("PRIVMSG: <" .. nick .. "> " .. msg )
 		local command, _, args = string.match(msg, "([^ ]*)( ?)(.*)")
 		if command ~= nil and args ~= nil then
 		  for _,mod in pairs(self.modules) do
@@ -68,10 +76,11 @@ local botirc = {
   end,
 
   step = function( self )
+    if self.server then
 	  local recv = self.server:receive()
 	  if recv ~= nil then
 		local comando = util.ircparse(recv)
-		print("UNFORMATTED: " .. recv)
+		self.notify_unformatted("UNFORMATTED: " .. recv)
 		local nick, msg = string.match(recv,"^:(.*)!.*(:.*)")
 		if util.contains_key( self.do_irc, comando[1]) then
 		  self.do_irc[comando[1] ]( self, comando[0], comando[2], comando[3] )
@@ -88,14 +97,15 @@ local botirc = {
 	  	  mod:cleanup()
 		end
 		self:send_msg( "PART " .. botdata.channel .. " :xxa1drp" )
-	  end
+  end
+  end
   end,
 
   -- main IRC loop
   loop = function( self )
-	while not self.exit do
-		self:step()
-	end
+    while not self.exit do
+      self:step()
+    end
   end,
 
   -- MODULE HANDLING --------------------------------------
